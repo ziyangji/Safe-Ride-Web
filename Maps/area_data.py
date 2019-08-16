7f240e11c53f1212fed189aab4befe3773d4e9ff
Donglin Liu <liudonglin301@gmail.com>
2019-08-16 18:48:23 -0400

    some data

---
+++ Maps/area_data.py
@@ -0,0 +1,8 @@
+# It is a file to store the longitude and altitude of the edge of
+#	serving area for future google mapping. The order is from up
+#	 left corner, clockwise 
+
+data_area = [(42.7476560, -73.68140550), (42.7476560, -73.6574369), 
+			(42.7356354, -73.6584133), (42.7121860, -73.6651292), 
+			(42.7086123, -73.6741186), (42.7136057, -73.6984043), 
+			(42.7284725, -73.6949966), (42.7394849, -73.6856349),]
